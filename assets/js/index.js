8267096c544480e4a2dce0e67d79920098757783
kam6y <daiyakanba0305@gmail.com>
2023-11-18 22:19:42 +0900

    メインビジュアル下書き完成

--- assets/js/index.js
+++ assets/js/index.js
@@ -16,3 +16,16 @@ $(window).on('load',function(){
   //=====ここまで背景が伸びた後に動かしたいJSをまとめる
       
 });
+
+// メインビジュアルの要素を取得
+const mainVisual = document.getElementById("js-mainVisual");
+
+// スクロールした時の処理
+window.addEventListener("scroll", () => {
+  // [スクロールした分の高さ] が [メインビジュアルの高さ - ヘッダーの高さ] より大きい時
+  if (window.scrollY > mainVisual.clientHeight) {
+    header.classList.remove("is-transparent");
+  } else {
+    header.classList.add("is-transparent");
+  }
+});
